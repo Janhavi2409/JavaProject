88a2af51f146318a42b894dc6a0d23a09ed09cd7
Janhavi <janhavidahatonde@gmail.com>
2023-09-10 18:27:11 +0530

    Generics demo

---
+++ src/GenericsInJava/Test.java
@@ -0,0 +1,16 @@
+package GenericsInJava;
+class myGen<T>{
+    T x;
+    static int count;
+
+    public myGen() {
+        count++;
+    }
+}
+public class Test {
+    public static void main(String[] args) {
+        myGen<Integer> m1 = new myGen<>();
+        myGen<Integer> m2 = new myGen<>();
+        System.out.println(myGen);
+    }
+}
