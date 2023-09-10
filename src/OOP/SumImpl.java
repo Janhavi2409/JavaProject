71639884b94bd853ab7188dc428e7c539755e5a6
Janhavi <janhavidahatonde@gmail.com>
2023-09-10 18:45:58 +0530

    Polymorphism

---
+++ src/OOP/SumImpl.java
@@ -0,0 +1,24 @@
+package OOP;
+
+class Sum {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+
+    public int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public double sum(double a, double b) {
+        return a + b;
+    }
+}
+
+public class SumImpl {
+    public static void main(String[] args) {
+        Sum s1 = new Sum();
+        System.out.println(s1.sum(4, 6));
+        System.out.println(s1.sum(3, 5, 7));
+        System.out.println(s1.sum(3.4, 7.9));
+    }
+}
