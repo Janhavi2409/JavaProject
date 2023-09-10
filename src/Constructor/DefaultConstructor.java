9909babf1de6cd37e5a462f88c5d4fa1dceb9427
Janhavi <janhavidahatonde@gmail.com>
2023-09-10 19:07:43 +0530

    Default constructor

---
+++ src/Constructor/DefaultConstructor.java
@@ -0,0 +1,11 @@
+package Constructor;
+class DConstructor{
+    public DConstructor(){
+        System.out.println("In default constructor");
+    }
+}
+public class DefaultConstructor {
+    public static void main(String[] args) {
+        DConstructor d = new DConstructor();
+    }
+}
