4e07e5c92cb048695df9b697df7cc662acf35e6f
Janhavi <janhavidahatonde@gmail.com>
2023-09-10 18:54:48 +0530

    Check whether leap year

---
+++ src/ConditionalStatements/LeapYear.java
@@ -0,0 +1,18 @@
+package ConditionalStatements;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = sc.nextInt();
+        if (year % 4 == 0 && year % 100 != 0) {
+            System.out.print(year + " is a leap year");
+        } else if (year % 400 == 0) {
+            System.out.print(year + " is a leap year");
+        } else {
+            System.out.print(year + " is not a leap year");
+        }
+    }
+}
