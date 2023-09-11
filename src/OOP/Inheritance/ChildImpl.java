9e9b418bcce0fc361f525c240d99877bd22b03ba
Janhavi <janhavidahatonde@gmail.com>
2023-09-11 16:14:37 +0530

    Many to one inheritance

--- src/OOP/Inheritance/ChildImpl.java
+++ src/OOP/Inheritance/ChildImpl.java
@@ -1,11 +1,14 @@
-package OOP;
-interface Mother{
+package OOP.Inheritance;
+
+interface Mother {
     void printMother();
 }
-interface Father{
+
+interface Father {
     void printFather();
 }
-class Child implements Mother, Father{
+
+class Child implements Mother, Father {
 
     @Override
     public void printMother() {
@@ -17,6 +20,7 @@ class Child implements Mother, Father{
         System.out.println("In father interface");
     }
 }
+
 public class ChildImpl {
     public static void main(String[] args) {
         Child child = new Child();
