5619a8acb9714d8cc5ddda0e638ea479c5ed1b60
Janhavi <janhavidahatonde@gmail.com>
2023-09-13 11:15:52 +0530

    Arithmetic Exception

---
+++ src/ExceptionHandling/DivideTwoNums.java
@@ -0,0 +1,20 @@
+package ExceptionHandling;
+
+import java.util.Scanner;
+
+public class DivideTwoNums {
+    public static void main(String[] args) {
+        try {
+            Scanner sc = new Scanner(System.in);
+            String[] arr = new String[2];
+            for (int i = 0; i < 2; i++) {
+                System.out.print("Enter element " + (i + 1) + ": ");
+                arr[i] = sc.next();
+            }
+            int div = Integer.parseInt(arr[0]) / Integer.parseInt(arr[1]);
+            System.out.println(arr[0] + " / " + arr[1] + " = " + (div));
+        } catch (NumberFormatException | ArithmeticException e) {
+            e.printStackTrace();
+        }
+    }
+}
