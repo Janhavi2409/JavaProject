1324d8ad62586991a463cd8d0f504505cace9bb6
Janhavi <janhavidahatonde@gmail.com>
2023-10-13 20:15:17 +0530

    Quick sort

--- src/Sorting/QuickSort.java
+++ src/Sorting/QuickSort.java
@@ -1,53 +1,42 @@
 package Sorting;
 
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class QuickSort {
-    static int partion(int[] arr, int low, int high) {
-        int pivot = arr[high];
-        int i = low - 1;
-        for (int j = low; j < high; j++) {
-            if (arr[j] < pivot) {
-                i++;
-                int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-        i++;
-        int temp = arr[i];
-        arr[i] = arr[high];
-        arr[high] = temp;
-        return i;
-    }
 
-    static int[] quickSort(int[] arr, int low, int high) {
-        if (low < high) {
-            int pidex = partion(arr, low, high);
-            quickSort(arr, low, pidex - 1);
-            quickSort(arr, pidex + 1, high);
+    static void quickSort(int[] array, int low, int high) {
+        if (low >= high) {
+            return;
         }
-        return arr;
+        int start = low;
+        int end = high;
+        int mid = start + (end - start) / 2;
+        int pivot = array[mid];
+        while (start <= end) {
+            while (array[start] < pivot) {
+                start++;
+            }
+            while (array[end] > pivot) {
+                end--;
+            }
+            if (start <= end) {
+                int temp = array[start];
+                array[start] = array[end];
+                array[end] = temp;
+                start++;
+                end--;
+            }
+            quickSort(array, low, end);
+            quickSort(array, start, high);
+        }
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter size of array: ");
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            System.out.print("Enter element " + i + ": ");
-            arr[i] = sc.nextInt();
-        }
+        int[] array = {12, 46, 43, -21, 78, 39, 79};
         System.out.print("Array before sorting: ");
-        for (int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
-        System.out.println();
-        int[] ans = quickSort(arr, 0, n - 1);
+        System.out.println(Arrays.toString(array));
         System.out.print("Array after sorting: ");
-        for (int i = 0; i < n; i++) {
-            System.out.print(ans[i] + " ");
-        }
+        quickSort(array, 0, array.length - 1);
+        System.out.println(Arrays.toString(array));
     }
 }
